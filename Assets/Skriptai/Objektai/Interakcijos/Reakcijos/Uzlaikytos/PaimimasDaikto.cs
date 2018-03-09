660ceff502dc0e7d4cf239be7f4ff15ace3ef9fe
ConsciousTank <36234023+ConsciousTank@users.noreply.github.com>
2018-03-09 15:21:04 +0200

    Pakeista repository vieta

---
+++ Assets/Skriptai/Objektai/Interakcijos/Reakcijos/Uzlaikytos/PaimimasDaikto.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PaimimasDaikto : PaveluotaReakcija
+{
+    public Daiktas daiktas;
+
+    private Inventorius inventorius;
+
+    protected override void SpecificInit()
+    {
+        inventorius = FindObjectOfType<Inventorius>();
+    }
+
+    protected override void ImmediateReaction()
+    {
+        inventorius.PridetiDaikta(daiktas);
+    }
+}
