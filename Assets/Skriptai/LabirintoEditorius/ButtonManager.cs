fee4c9bb81e987565d8dce2aafbfac68c1973ced
ConsciousTank <36234023+ConsciousTank@users.noreply.github.com>
2018-03-21 19:34:10 +0200

    Daugiau failų prie Labirinto Editoriaus
    Pridėta klasių(ButtonManager.cs, SampleKambarys.cs), nauja scena labirinto kūrimui su panele ir mygtukų listu)

---
+++ Assets/Skriptai/LabirintoEditorius/ButtonManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+[System.Serializable]
+public class Room
+{
+    public string itemName;
+    public Sprite icon;
+    public int id = 1;
+    public GameObject room = null;
+}
+
+public class ButtonManager : MonoBehaviour {
+
+    public GameObject prefab;
+    public List<Room> rooms = new List<Room>();
+    public List<GameObject> RoomsM = new List<GameObject>();
+    public GameObject Panel;
+
+
+	void Start () {
+        AddRooms();
+	}
+	
+    public void AddRooms()
+    {
+        for (int i = 0; i < rooms.Count; i++)
+        {
+            Room room = rooms[i];
+            GameObject newRoom = Instantiate(prefab);
+            newRoom.transform.SetParent(Panel.transform);
+            SampleKambarys kamba = newRoom.GetComponent<SampleKambarys>();
+            kamba.Setup(room, this);
+        }
+    }
+
+
+}
---
+++ Assets/Skriptai/LabirintoEditorius/SampleKambarys.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class SampleKambarys : MonoBehaviour {
+    public Button buttonComponent;
+    public string nameLabel;
+    public Sprite iconImage;
+    public int id;
+    public GameObject roomM;
+
+
+    private Room room;
+    private ButtonManager BuMa;
+
+    // Use this for initialization
+    void Start () {
+        buttonComponent.onClick.AddListener(HandleClick);
+    }
+
+    public void Setup(Room currentRoom, ButtonManager BuMa)
+    {
+        room = currentRoom;
+        nameLabel = room.itemName;
+        iconImage = room.icon;
+        id = room.id;
+        roomM = currentRoom.room;
+        this.BuMa = BuMa;
+        
+
+    }
+    public void HandleClick()
+    {
+        Instantiate(roomM);
+    }
+}
