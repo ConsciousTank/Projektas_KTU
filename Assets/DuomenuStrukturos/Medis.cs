5221986d2ede3f73f60e92590d2cc9708a31a03c
ConsciousTank <36234023+ConsciousTank@users.noreply.github.com>
2018-03-28 13:06:28 +0300

    Pridėtos nauja duomenų struktūros klasė
    Pridėta Medis.cs, dirbta prie Inventoriaus klasių

---
+++ Assets/DuomenuStrukturos/Medis.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+
+public class Medis<tipas> : ScriptableObject where tipas: IComparable
+{
+    private sealed class Nuoroda<tipas> where tipas: IComparable
+    {
+        public tipas duomenis;
+        public Nuoroda<tipas> kaire;
+        public Nuoroda<tipas> desine;
+        
+        public Nuoroda()
+        {
+        }
+
+        public Nuoroda(tipas duomenis)
+        {
+            this.duomenis = duomenis;
+            kaire = null;
+            desine = null;
+        }
+
+        public Nuoroda(tipas duomenis, Nuoroda<tipas> kaire, Nuoroda<tipas> desine)
+        {
+            this.duomenis = duomenis;
+            this.kaire = kaire;
+            this.desine = desine;
+        }
+    }
+
+    private Nuoroda<tipas> virsune;
+    private Nuoroda<tipas> kaire;
+    private Nuoroda<tipas> desine;
+
+    public Medis()
+    {
+        virsune = null;
+        kaire = null;
+        desine = null;
+    }
+
+    public void Prideti(tipas duomenis)
+    {
+
+    }
+}
--- Assets/Skriptai/Inventorius/Objektai/Inventorius/Daiktas.cs
+++ Assets/Skriptai/Inventorius/Objektai/Inventorius/Daiktas.cs
@@ -3,5 +3,54 @@
 [CreateAssetMenu]
 public class Daiktas : ScriptableObject
 {
-    public Sprite daiktoPaveiksliukas;
+    public static int visoDaiktu;
+    private Sprite daiktoPaveiksliukas;
+    private string pavadinimas;
+    private string aprasymas;
+    private int laukelisPriklausimo;
+
+    public Daiktas()
+    {
+
+    }
+
+    public Daiktas(Sprite paveiksliukas, string pavadinimas, string aprasymas, int laukelis)
+    {
+        paveiksliukas = daiktoPaveiksliukas;
+        this.pavadinimas = pavadinimas;
+        this.aprasymas = aprasymas;
+        laukelis = laukelisPriklausimo;
+    }
+
+    public string Pavadinimas
+    {
+        get
+        {
+            return pavadinimas;
+        }
+    }
+
+    public string Aprasymas
+    {
+        get
+        {
+            return aprasymas;
+        }
+    }
+
+    public int LaukelisPaskirties
+    {
+        get
+        {
+            return laukelisPriklausimo;
+        }
+    }
+
+    public Sprite Paveiksliukas
+    {
+        get
+        {
+            return daiktoPaveiksliukas;
+        }
+    }
 }
